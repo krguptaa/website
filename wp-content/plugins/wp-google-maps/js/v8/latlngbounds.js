2b5c50de6aca5767036245c4a32652ff6b630e0c
kamlesh401 <kamlesh.gupta@cygnetinfotech.com>
2019-01-25 17:08:18 +0530

    Update content

---
+++ wp-content/plugins/wp-google-maps/js/v8/latlngbounds.js
@@ -0,0 +1,62 @@
+/**
+ * @namespace WPGMZA
+ * @module LatLngBounds
+ * @requires WPGMZA
+ */
+jQuery(function($) {
+	
+	/**
+	 * This class represents latitude and longitude bounds as a rectangular area.
+	 * NB: This class is not fully implemented
+	 * @class WPGMZA.LatLngBounds
+	 * @constructor WPGMZA.LatLngBounds
+	 * @memberof WPGMZA
+	 */
+	WPGMZA.LatLngBounds = function(southWest, northEast)
+	{
+		
+	}
+	
+	/**
+	 * Returns true if this object is in it's initial state (eg no points specified to gather bounds from)
+	 * @method
+	 * @memberof WPGMZA.LatLngBounds
+	 * @return {bool} True if the object is in it's initial state
+	 */
+	WPGMZA.LatLngBounds.prototype.isInInitialState = function()
+	{
+		return (this.north == undefined && this.south == undefined && this.west == undefined && this.east == undefined);
+	}
+	
+	/**
+	 * Extends this bounds object to encompass the given latitude and longitude coordinates
+	 * @method
+	 * @memberof WPGMZA.LatLngBounds
+	 * @param {object|WPGMZA.LatLng} latLng either a LatLng literal or an instance of WPGMZA.LatLng
+	 */
+	WPGMZA.LatLngBounds.prototype.extend = function(latLng)
+	{
+		if(!(latLng instanceof WPGMZA.LatLng))
+			latLng = new WPGMZA.LatLng(latLng);
+		
+		if(this.isInInitialState())
+		{
+			this.north = this.south = latLng.lat;
+			this.west = this.east = latLng.lng;
+			return;
+		}
+		
+		if(latLng.lat < this.north)
+			this.north = latLng.lat;
+		
+		if(latLng.lat > this.south)
+			this.south = latLng.lat;
+		
+		if(latLng.lng < this.west)
+			this.west = latLng.lng;
+		
+		if(latLng.lng > this.east)
+			this.east = latLng.lng;
+	}
+	
+});
